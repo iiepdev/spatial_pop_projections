4ea5cd6b4d56df3de95f313134edc8b12c876aae
eemilhaa <eemil.haapanen@gmail.com>
2023-05-25 08:49:06 +0000

    exclude tests from formatting, add task for testing

--- scripts/data_analysis/reshape.py
+++ scripts/data_analysis/reshape.py
@@ -3,7 +3,7 @@ import numpy as np
 
 def shape_image_array_to_time_series(image_array: np.ndarray, n_observations: int):
     """Shapes a 3-D array of images to 2D.
-    
+
     Stacks an array of images along its 3rd dimenstion and reshapes the stacked
     array. Each element of the reshaped array contains a time series of values
     of a given pixel in the input 3-D array.
@@ -11,7 +11,7 @@ def shape_image_array_to_time_series(image_array: np.ndarray, n_observations: in
     Args:
         image_array: A 3-D array containing images
         n_observations: The number of observations (the number of images)
-    
+
     Returns:
         time_series: The reshaped array
     """
